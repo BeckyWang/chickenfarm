a16ef15a2a0cd7642b289737c3d7414ff8cf1385
BeckyWang <wangbeibei01018@163.com>
2017-08-28 16:18:57 +0800

    我的客户分享

--- src/components/PersonalCenter/Client.js
+++ src/components/PersonalCenter/Client.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { WingBlank, WhiteSpace, Icon, Button, Result, Flex } from 'antd-mobile';
+import { WingBlank, WhiteSpace, Icon, Button, Result, Flex, Toast } from 'antd-mobile';
 
 import { randomchar } from '../../unit/tool';
 import { getMyClients, getWXSignature, getJsapiTicket } from '../../unit/fetch';
@@ -49,25 +49,31 @@ class Client extends React.Component {
                 });
                 
                 wx.config({
-                    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                     appId: 'wx20cc91f559b59b67', // 必填，公众号的唯一标识
                     timestamp, // 必填，生成签名的时间戳
                     nonceStr, // 必填，生成签名的随机串
                     signature,// 必填，签名，见附录1
-                    jsApiList: ['onMenuShareTimeline', 'onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                    jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
                 });
 
                 wx.ready(() => {
                     wx.onMenuShareTimeline({
-                        title: '分享到朋友圈', // 分享标题
-                        link: `http://www.chickenfarm.com.cn/weixin/cultivation/toFarms.html?referrer=${openId}`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                        imgUrl: '', // 分享图标
+                        title: '土吉蛋——我的客户', // 分享标题
+                        link: `http://www.chickenfarm.com.cn/weixin/toFarms.html?referrer=${openId}`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                        imgUrl: 'http://www.chickenfarm.com.cn/weixin/image/logo.png', // 分享图标
                         success: res => { // 用户确认分享后执行的回调函数
-                            console.log(res);
+                            Toast.success('分享成功！', 2);
                         },
-                        cancel: () => { // 用户取消分享后执行的回调函数
-                        
-                        }
+                    });
+
+                    wx.onMenuShareAppMessage({
+                        title: '土吉蛋——我的客户', // 分享标题
+                        link: `http://www.chickenfarm.com.cn/weixin/toFarms.html?referrer=${openId}`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                        imgUrl: 'http://www.chickenfarm.com.cn/weixin/image/logo.png', // 分享图标
+                        success: res => { // 用户确认分享后执行的回调函数                            
+                            Toast.success('分享成功！', 2);
+                        },
                     });
                 });
             } catch(e) {

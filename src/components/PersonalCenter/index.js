7932e4a1311b213761c4766e7fd2766102176eb7
VieZhong <zyw13579@qq.com>
2017-10-17 21:48:45 +0800

    改了等于没改

--- src/components/PersonalCenter/index.js
+++ src/components/PersonalCenter/index.js
@@ -70,7 +70,7 @@ class PersonalCenter extends React.Component {
                         timestamp, // 必填，生成签名的时间戳
                         nonceStr, // 必填，生成签名的随机串
                         signature,// 必填，签名，见附录1
-                        jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                        jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
                     });
 
                     wx.ready(() => {
